2a468c8d9725e5a0d7a4cab4da306f505d24ccf9
kumarpawar59@gmail.com <kumarpawar59@gmail.com>
2021-07-29 08:43:47 +0530

    Second Step.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  return (
+    <div>
+       <h1>hello World......</h1>
+       <h1>Hello universe
+    </h1>
+    </div>
+   
+  );
 
 }
 
