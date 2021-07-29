afb96644fb7ec2186dd353c32e4ec428b455fc41
kumarpawar59@gmail.com <kumarpawar59@gmail.com>
2021-07-29 08:54:20 +0530

    Third Step.

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  let id = "100";
+  let name = "kumar";
+  let city = "pune";
   return (
     <div>
-       <h1>hello World......</h1>
-       <h1>Hello universe
-    </h1>
+      <h1>{name},{city}</h1>
+      <h1>{id}</h1>
     </div>
-   
   );
-
 }
 
 export default App;
