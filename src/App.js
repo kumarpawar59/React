c4e506cd50231abf568fc3493264c7625fac0050
kumarpawar59@gmail.com <kumarpawar59@gmail.com>
2021-07-29 22:12:18 +0530

    third.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,28 @@ import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
 
+
 function App() {
- 
+
+  //stateless member 
+  let counter = 100;
+
+
+
+  let increment = () => {
+    counter += 1;
+    console.log(counter);
+  };
+
   return (
-    <h1 className="bg-primary my-4 p-3 text-light ">Hello India....</h1>
+    <div>
+      <h1 className="bg-primary p-2 my-3">Second p1</h1>
+      {counter}
+      <input type="button" value="Counter" onClick={increment} />
+    </div>
   );
 }
 
 export default App;
+
+
