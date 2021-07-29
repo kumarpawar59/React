50ec9f18318e2d0933a2ae78d934937e3070fec2
kumarpawar59@gmail.com <kumarpawar59@gmail.com>
2021-07-29 22:33:20 +0530

    fouth.

--- src/App.js
+++ src/App.js
@@ -2,28 +2,23 @@ import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
 
-
 function App() {
-
-  //stateless member 
-  let counter = 100;
-
-
-
-  let increment = () => {
-    counter += 1;
-    console.log(counter);
-  };
+  let [name] = useState("kumar");
+  let [counter] = useState(100);
+  let [active] = useState(true);
+  let [obj] = useState({ name: "nam" });
+  let [list] = useState(["Akshay", "Raj"]);
 
   return (
-    <div>
-      <h1 className="bg-primary p-2 my-3">Second p1</h1>
-      {counter}
-      <input type="button" value="Counter" onClick={increment} />
+    <div className="p-4 ">
+      <h1>String  :: {name}</h1>
+      <h1>number  :: {counter}</h1>
+      <h1>Boolean :: {active +""}</h1>
+      <h1>object  :: {JSON.stringify(obj)}</h1>
+      <h1> Array  :: {list}</h1>
+      <h1>obj   :: {obj.name}</h1>
     </div>
   );
 }
 
 export default App;
-
-
